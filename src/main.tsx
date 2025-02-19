0a33b8d7fe6f37e60c106bfc7673934a92100377
Mahmood Sagharjooghi <mahmood.sagharjooghi@oneflow.com>
2025-02-20 00:37:19 +0100

    add persistent storage

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
-import { QueryClientProvider } from '@tanstack/react-query'
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client'
+
 
 import { routeTree } from './routeTree.gen'
 
 import './main.css'
 import { HotkeyProvider } from './contexts/hotkey-context'
-import { queryClient } from './query-client'
+import { queryClient, asyncStoragePersister } from './query-client'
 
 // Create a new router instance
 const router = createRouter({ routeTree })
@@ -21,10 +22,10 @@ declare module '@tanstack/react-router' {
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
+    <PersistQueryClientProvider client={queryClient} persistOptions={{ persister: asyncStoragePersister }}>
       <HotkeyProvider>
         <RouterProvider router={router} />
       </HotkeyProvider>
-    </QueryClientProvider>
+    </PersistQueryClientProvider>
   </StrictMode>,
 )
--- src/query-client.ts
+++ src/query-client.ts
@@ -1,4 +1,5 @@
-
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { createAsyncStoragePersister } from '@tanstack/query-async-storage-persister'
 import { QueryClient } from '@tanstack/react-query'
 
 export const queryClient = new QueryClient({
@@ -11,6 +12,11 @@ export const queryClient = new QueryClient({
       // 1 hour
       staleTime: 1000 * 60 * 60,
       gcTime: 1000 * 60 * 60,
+      cacheTime: 1000 * 60 * 60 * 24, // 24 hours
     },
   },
+})
+
+export const asyncStoragePersister = createAsyncStoragePersister({
+  storage: AsyncStorage,
 })
